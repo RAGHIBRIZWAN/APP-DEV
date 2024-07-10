b581eab6c9288a81952340686403e2a89ac5cf50
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-07-10 19:18:26 +0500

    Create assets.dart

---
+++ Flappy Bird Game/assets.dart
@@ -0,0 +1,10 @@
+class Assets{
+  static const background = 'background.png';
+  static const ground = 'ground.png';
+  static const downPipe = 'down pipe.png';
+  static const upPipe = 'up pipe.png';
+  static const gameOver = 'gameOver.png';
+  static const birdImage = 'birdImage.png';
+  static const menu = 'menu.png';
+  static const logo = 'RRR LOGO real.png';
+}
